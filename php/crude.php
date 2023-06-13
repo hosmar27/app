b7dbb7bc1db9b72b723d7a5c3541fca3bc814160
hosmar27 <hosmaradelino@gmail.com>
2023-06-13 20:38:09 -0300

    arrumado o crude e pacotes

--- php/crude.php
+++ php/crude.php
@@ -6,7 +6,13 @@
     $email = $_POST["email"];
     $senha = $_POST["senha"];
 
-    $comando = $pdo->prepare("INSERT INTO usuario (nome, cpf, email, senha) VALUES('$nome','$cpf', '$email', '$senha')");
+    $comando = $pdo->prepare("INSERT INTO usuario (nome, cpf, email, senha) VALUES(:nome,:cpf, :email, :senha)");
+
+    $comando->bundParam(':nome',$nome);
+    $comando->bundParam(':cpf',$cpf);
+    $comando->bundParam(':email',$email);
+    $comando->bundParam(':senha',$senha);
+
     $resultado = $comando->execute();
     header("Location: ../html/index.html");
 
