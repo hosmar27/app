3e1c1c7b4f980ce7ffb5e3b9188465140e4866bd
hosmar27 <hosmaradelino@gmail.com>
2023-06-21 11:44:13 -0300

    banco de dados em andamento

--- sql/trailermarine.sql
+++ sql/trailermarine.sql
@@ -4,20 +4,25 @@ USE trailermarine;
 
 CREATE TABLE IF NOT EXISTS usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    cpf VARCHAR(14) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    admin varchar(1) NOT null
+    nome VARCHAR(255),
+    cpf VARCHAR(14),
+    email VARCHAR(255),
+    senha VARCHAR(255),
+    admin varchar(1)
 );
 
 CREATE TABLE IF NOT EXISTS pacote (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR (100) NOT NULL,
-    valor VARCHAR (10) NOT NULL,
+    nome VARCHAR (100),
+    valor decimal (10,2),
+    descricao varchar (255) not null,
     imagem mediumblob NOT NULL
 );
 
-INSERT INTO pacote (nome,valor) values(
-    'pacote barbados','R$5.000,00'
+INSERT INTO usuario (nome,cpf,email,senha,admin) values(
+    'osmar','12345678901','osmar_email@email.com','12345','s'
+);
+
+INSERT INTO pacote (nome,valor,descricao) values(
+    'pacote barbados','5.000,00','passeio de 3 dias no mar de barbados'
 );
